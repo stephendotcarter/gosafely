11e1a586f0656d6d7ec57039f9dd0debd155c6b9
Stephen Carter <stephencarter@gmail.com>
2018-11-04 22:33:14 +0000

    Add download command

--- cmd/gosafely/gosafely.go
+++ cmd/gosafely/gosafely.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
+	"github.com/manifoldco/promptui"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 
@@ -16,7 +19,7 @@ var (
 	apiURL       = os.Getenv("SS_API_URL")
 	apiKeyID     = os.Getenv("SS_API_KEY_ID")
 	apiKeySecret = os.Getenv("SS_API_KEY_SECRET")
-	api          *gosafely.API
+	ssAPI        *gosafely.API
 	ssURL        string
 )
 
@@ -37,7 +40,7 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List the files in a package",
 	Run: func(cmd *cobra.Command, args []string) {
-		p, err := getPackage(ssURL)
+		p, _, err := getPackage(ssURL)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -46,17 +49,89 @@ var listCmd = &cobra.Command{
 	},
 }
 
-func getPackage(packageURL string) (gosafely.Package, error) {
+var downloadCmd = &cobra.Command{
+	Use:   "download",
+	Short: "Download the files in a package",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		p, pm, err := getPackage(ssURL)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		printPackage(p)
+
+		selected, err := getDownloadIndices(len(p.Files))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println("")
+		for _, s := range selected {
+			fp := "./" + p.Files[s].FileName
+			fmt.Printf("Downloading %s\n", p.Files[s].FileName)
+			err = ssAPI.DownloadFile(pm, p, p.Files[s], fp, gosafely.ProgressPrintBytes)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println()
+		}
+	},
+}
+
+func getDownloadIndices(fc int) ([]int64, error) {
+	validate := func(input string) error {
+		_, err := getIndices(input, fc)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Files",
+		Validate: validate,
+	}
+	result, err := prompt.Run()
+
+	selected, err := getIndices(result, fc)
+	if err != nil {
+		return selected, err
+	}
+	return selected, nil
+}
+
+func getIndices(input string, fc int) ([]int64, error) {
+	input = strings.Replace(input, " ", "", -1)
+	selected := []int64{}
+	for _, i := range strings.Split(input, ",") {
+		ip, err := strconv.ParseInt(i, 10, 64)
+		if err != nil {
+			return nil, errors.New("Index must be a number")
+		}
+		if ip >= int64(fc) {
+			return nil, fmt.Errorf("Index must be between %d and %d", 0, fc-1)
+		}
+		selected = append(selected, ip)
+	}
+
+	return selected, nil
+}
+
+func getPackage(packageURL string) (gosafely.Package, gosafely.PackageMetadata, error) {
 	var p gosafely.Package
-	pm, err := api.GetPackageMetadataFromURL(packageURL)
+	var pm gosafely.PackageMetadata
+	pm, err := ssAPI.GetPackageMetadataFromURL(packageURL)
 	if err != nil {
-		return p, err
+		return p, pm, err
 	}
-	p, err = api.GetPackage(pm.PackageCode)
+	p, err = ssAPI.GetPackage(pm.PackageCode)
 	if err != nil {
-		return p, err
+		return p, pm, err
 	}
-	return p, nil
+	return p, pm, nil
 }
 
 func printPackage(p gosafely.Package) {
@@ -92,13 +167,17 @@ func init() {
 		os.Exit(1)
 	}
 
-	api = gosafely.NewAPI(apiURL, apiKeyID, apiKeySecret)
+	ssAPI = gosafely.NewAPI(apiURL, apiKeyID, apiKeySecret)
 
 	rootCmd.AddCommand(versionCmd)
 
 	listCmd.Flags().StringVarP(&ssURL, "url", "u", "", "SendSafely URL to query")
 	listCmd.MarkFlagRequired("url")
 	rootCmd.AddCommand(listCmd)
+
+	downloadCmd.Flags().StringVarP(&ssURL, "url", "u", "", "SendSafely URL to query")
+	downloadCmd.MarkFlagRequired("url")
+	rootCmd.AddCommand(downloadCmd)
 }
 
 func execute() {
